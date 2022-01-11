a6b8e220d616b8e3cdfe6d54223da5a507b288f6
Ahmad Wahyudi <84139744+wahyu9kdl@users.noreply.github.com>
2022-01-11 07:04:10 +0700

    Create bower.js

---
+++ bower.js
@@ -0,0 +1,19 @@
+{
+  "name": "animation",
+  "description": "A simple and easy jQuery plugin for CSS animated page transitions.",
+  "keywords": [
+    "css3",
+    "js",
+    "animation",
+    "page transition",
+    "jQuery"
+  ],
+  "homepage": "http://git.wahyu9kdl.com/animation",
+  "main": [
+    "dist/css/animsition.min.css",
+    "dist/js/animsition.min.js"
+  ],
+  "ignore": [
+    ".*"
+  ]
+}
